d2aa2604dbbaeb3d93898ba7519c05063f1f7194
josiyuh <josiahhorn23@gmail.com>
2026-02-03 10:07:34 -0600

    Josiah Horn Lab01 Passed Code.

--- lab01/src/three_sum_sorted.c
+++ lab01/src/three_sum_sorted.c
@@ -1,8 +1,31 @@
 #include "three_sum_sorted.h"
 
 int three_sum_zero_sorted_one(const int* nums, int n, int* out_i, int* out_j, int* out_k) {
-    
+
     // implement your code here
+    if (!nums || !out_i || !out_j || !out_k || n < 3) {
+        return 0;
+    }
+
+    for (int i = 0; i < n - 2; i++) {
+        int left = i + 1;
+        int right = n - 1;
+
+        while (left < right) {
+            long sum = (long)nums[i] + (long)nums[left] + (long)nums[right];
+
+            if (sum == 0) {
+                *out_i = i;
+                *out_j = left;
+                *out_k = right;
+                return 1;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
 
     return 0;
 }
--- lab01/src/two_sum_sorted.c
+++ lab01/src/two_sum_sorted.c
@@ -4,8 +4,23 @@ int two_sum_sorted(const int* nums, int n, int target, int* out_i, int* out_j) {
     if (!nums || !out_i || !out_j || n < 2) return 0;
 
     // TODO: implement rwo sum solution for sorted arrays.
+    int left = 0;
+    int right = n - 1;
+
+    while (left < right) {
+        long sum = (long)nums[left] + (long)nums[right];
+
+        if (sum == target) {
+            *out_i = left;
+            *out_j = right;
+            return 1;
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
 
     return 0;
 }
 // think about the time complexity and space complexity of your solution
-
