f2c41eee81da246248ee95d55454da487bbd619f
josiyuh <josiahhorn23@gmail.com>
2026-02-20 10:33:39 -0600

    "Finished Assignment 1"

--- Assignment1/Question1/student.c
+++ Assignment1/Question1/student.c
@@ -17,6 +17,22 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
 
  // TODO: implement
 
+    (void)nums1Size;  // sizes may be unused by some solutions/tests
+    (void)nums2Size;
 
+    int i = m - 1;        // last real element index in nums1
+    int j = n - 1;        // last element index in nums2
+    int k = m + n - 1;    // last slot in nums1
 
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    while (j >= 0) {
+        nums1[k--] = nums2[j--];
+    }
 }
