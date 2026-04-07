c137623227d489676d9fcad0024825fb37526e1e
Josiah Horn <josiahhorn23@gmail.com>
2026-04-06 20:58:52 -0500

    "Corrected code lab05, and lab06"

--- lab05/Question1/student.c
+++ lab05/Question1/student.c
@@ -1,32 +1,3 @@
-/*
-Question 1: Two Sum
-
-Description:
-Given an array of integers nums and an integer target, return the indices
-of the two numbers such that they add up to target.
-
-You may assume that each input has exactly one solution, and you may not
-use the same element twice.
-
-For this lab, you should solve the problem using a hash-based idea.
-
-Function:
-int* twoSum(int* nums, int numsSize, int target, int* returnSize);
-
-Notes:
-- Return a dynamically allocated array of size 2.
-- Set *returnSize = 2 before returning.
-- The returned array will be freed by the caller.
-
-Example:
-Input:  nums = [2, 7, 11, 15], target = 9
-Output: [0, 1]
-
-Hint:
-As you scan the array, for each nums[i], think about whether the value
-(target - nums[i]) has already appeared before.
-*/
-
 #include <stdlib.h>
 
 /*
@@ -65,6 +36,27 @@ whose sum equals target.
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     /* Write your code here */
 
+    Node* table[TABLE_SIZE] = {0};
+
+    for (int i = 0; i < numsSize; i++) {
+        int complement = target - nums[i];
+        int foundIndex;
+
+        // Check if complement already exists
+        if (find(table, complement, &foundIndex)) {
+            int* result = (int*)malloc(2 * sizeof(int));
+            result[0] = foundIndex;
+            result[1] = i;
+            *returnSize = 2;
+
+            freeTable(table);
+            return result;
+        }
+
+        // Insert current number into hash table
+        insert(table, nums[i], i);
+    }
+
     *returnSize = 0;
     return NULL;
 }
@@ -74,7 +66,8 @@ Optional helper: compute a hash index for a key.
 */
 static int hash(int key) {
     /* Write your code here if you use this helper */
-    return 0;
+    if (key < 0) key = -key;
+    return key % TABLE_SIZE;
 }
 
 /*
@@ -82,6 +75,14 @@ Optional helper: insert (key, value) into the hash table.
 */
 static void insert(Node* table[], int key, int value) {
     /* Write your code here if you use this helper */
+    int index = hash(key);
+
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->key = key;
+    newNode->value = value;
+    newNode->next = table[index];
+
+    table[index] = newNode;
 }
 
 /*
@@ -91,6 +92,17 @@ Otherwise return 0.
 */
 static int find(Node* table[], int key, int* value) {
     /* Write your code here if you use this helper */
+    int index = hash(key);
+    Node* curr = table[index];
+
+    while (curr != NULL) {
+        if (curr->key == key) {
+            *value = curr->value;
+            return 1;
+        }
+        curr = curr->next;
+    }
+
     return 0;
 }
 
@@ -99,4 +111,12 @@ Optional helper: free all memory used by the hash table.
 */
 static void freeTable(Node* table[]) {
     /* Write your code here if you use this helper */
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        Node* curr = table[i];
+        while (curr != NULL) {
+            Node* temp = curr;
+            curr = curr->next;
+            free(temp);
+        }
+    }
 }
--- lab06/Question2/student.c
+++ lab06/Question2/student.c
@@ -1,33 +1,25 @@
 #include "graph.h"
-// Description
-
-// Write a function that counts how many nodes are isolated.
-// A node is isolated if it has no edges connected to it.
-// // The graph is stored as an adjacency matrix. 
-// Graph* g is a pointer to a Graph struct, which contains a 2D array adj[MAX_NODES][MAX_NODES]. 
-// The value at adj[u][v] is 1 if there is an edge between nodes u and v, and 0 otherwise.
-
-
-
 
 int count_isolated(Graph* g) {
     // TODO: implement
     // return -1;
-   
+
+    int isolatedCount = 0;
+
+    for (int i = 0; i < MAX_NODES; i++) {
+        int isIsolated = 1;
+
+        for (int j = 0; j < MAX_NODES; j++) {
+            if (g->adj[i][j] == 1) {
+                isIsolated = 0;
+                break;
+            }
+        }
+
+        if (isIsolated) {
+            isolatedCount++;
+        }
+    }
+
+    return isolatedCount;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Tip: In the adjacency matrix, that means the entire row for that node contains only 0s.
