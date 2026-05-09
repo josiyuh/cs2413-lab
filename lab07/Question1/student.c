20ee82652423ba8e3695d5f7121780edf0f831cc
Josiah Horn <josiahhorn23@gmail.com>
2026-05-08 20:57:16 -0500

    WORKING CODE

--- lab07/Question1/student.c
+++ lab07/Question1/student.c
@@ -28,15 +28,41 @@ Notes:
 - You may write a helper function such as swap(...) if you want.
 */
 
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void bubbleSort(int arr[], int size) {
     // TODO: implement basic bubble sort
-    (void)arr;
-    (void)size;
+
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
 }
 
 void bubbleSortOptimized(int arr[], int size) {
     // TODO: implement optimized bubble sort with early stopping
-    (void)arr;
-    (void)size;
-}
 
+    bool swapped;
+
+    for (int i = 0; i < size - 1; i++) {
+        swapped = false;
+
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped) {
+            break;
+        }
+    }
+}
--- lab07/Question2/student.c
+++ lab07/Question2/student.c
@@ -28,9 +28,67 @@ Notes:
 - You may use temporary arrays inside your merge function.
 */
 
-void mergeSort(int arr[], int size) {
-    // TODO: implement merge sort
-    (void)arr;
-    (void)size;
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int leftArr[n1];
+    int rightArr[n2];
+
+    for (int i = 0; i < n1; i++) {
+        leftArr[i] = arr[left + i];
+    }
+
+    for (int j = 0; j < n2; j++) {
+        rightArr[j] = arr[mid + 1 + j];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < n1 && j < n2) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i];
+            i++;
+        } else {
+            arr[k] = rightArr[j];
+            j++;
+        }
+
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
 }
 
+void mergeSortHelper(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSortHelper(arr, left, mid);
+        mergeSortHelper(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+void mergeSort(int arr[], int size) {
+    // TODO: implement merge sort
+
+    if (size <= 1) {
+        return;
+    }
+
+    mergeSortHelper(arr, 0, size - 1);
+}
